007058a997185517df6d1df6ff2be907ca16cf5d
unknown <levimlnr@gmail.com>
2024-03-18 16:28:59 +0100

    troubleshooting connection to socket

--- client/src/context/socket.tsx
+++ client/src/context/socket.tsx
@@ -3,12 +3,13 @@ import { io, Socket } from 'socket.io-client';
 
 
 const address = process.env['REACT_APP_SERVER_ADDRESS'] ?? "localhost";
-const port = process.env['REACT_APP_SERVER_PORT'] ?? "3001";
-const pagesUrl = process.env['REACT_APP_URL'];
+// const port = process.env['REACT_APP_SERVER_PORT'] ?? "3001";
 
-const socket = io(`http://${address}:${port}`);
-console.log(`http://${address}:${port}`);
-console.log("url: ", pagesUrl);
+// const socket = io(`http://${address}:${port}`);
+// console.log(`http://${address}:${port}`);
+
+const socket = io(`http://${address}`);
+console.log(`http://${address}`);
 
 export const SocketContext = createContext<Socket>(socket);
 
